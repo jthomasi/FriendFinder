95c29742161ce146bbbdee66c3057479b9256a9f
Jesse Thomas <jayayetea@gmail.com>
2017-04-19 20:51:10 -0500

    created routes for the json friends list to be displayed or updated

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,17 +1,14 @@
 var friendsList = require("../data/friends");
 
-console.log(friendsList);
-
 module.exports = function(app) {
 
 	app.get("/api/friends", function(req, res) {
 		res.json(friendsList);
-		console.log(friendsList);
 	});
 
-	// app.post("/api/friends", function(req, res) {
-	//     friendsList.push(req.body);
-	// });
+	app.post("/api/friends", function(req, res) {
+	    friendsList.push(req.body);
+	});
 
 };
 
