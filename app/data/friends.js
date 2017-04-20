ba090adfeec6ab1c5aacd0a966f3b6ba565423d0
Jesse Thomas <jayayetea@gmail.com>
2017-04-19 20:41:31 -0500

    initial array of friends

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendList = [
+	{
+		"name":"Jesse Thomas",
+		"photo":"http://0x0800.github.io/2048-CUPCAKES/style/img/1024.jpg",
+		"scores":[
+			5,
+			4,
+			3,
+			5,
+			1,
+			2,
+			4,
+			4,
+			1,
+			5
+		]
+	}
+];
+
+module.exports = friendList;
