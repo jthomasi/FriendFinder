40f8174b6a03976468d4d1ee9231b6f028738eb5
Jesse Thomas <jayayetea@gmail.com>
2017-04-22 21:08:35 -0500

    final bug fixes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
