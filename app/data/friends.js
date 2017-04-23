6ae9ce606f9b28dd973d319e9c2e957f917818c8
Jesse Thomas <jayayetea@gmail.com>
2017-04-22 20:19:50 -0500

    added modal and matching function to display users best match

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,16 +3,61 @@ var friendList = [
 		"name":"Jesse Thomas",
 		"photo":"http://0x0800.github.io/2048-CUPCAKES/style/img/1024.jpg",
 		"scores":[
-			5,
-			4,
-			3,
-			5,
-			1,
-			2,
-			4,
-			4,
-			1,
-			5
+			'5',
+			'4',
+			'3',
+			'5',
+			'1',
+			'2',
+			'4',
+			'4',
+			'1',
+			'5'
+		]
+	}, {
+		"name":"SpongeBob SquarePants",
+		"photo":"http://vignette4.wikia.nocookie.net/vsbattles/images/d/dd/2015-07-14-1436902565-6235018-SpongeBob_5.png/revision/latest?cb=20150919161402",
+		"scores":[
+			'3',
+			'4',
+			'3',
+			'2',
+			'1',
+			'2',
+			'4',
+			'3',
+			'1',
+			'3'
+		]
+	}, {
+		"name":"Joe Biden",
+		"photo":"http://cdn.hercampus.com/s3fs-public/2016/11/13/Joe-Biden.jpg",
+		"scores":[
+			'1',
+			'2',
+			'3',
+			'5',
+			'1',
+			'2',
+			'1',
+			'4',
+			'2',
+			'5'
+		]
+	}, {
+		"name":"Gus the Weenie Dog",
+		"photo":"https://i.imgur.com/DQDa9OE.jpg",
+		"scores":[
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'5',
+			'2',
+			'1',
+			'1',
+			'2'
 		]
 	}
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,9 +7,40 @@ module.exports = function(app) {
 	});
 
 	app.post("/api/friends", function(req, res) {
-	    friendsList.push(req.body);
-	    
-	});
 
+	    friendsList.push(req.body);
+	    var diffArray = [];
+		var currentUser = friendsList[friendsList.length-1].scores;
+
+		for (var i=0;i<friendsList.length-1;i++){
+			differences(currentUser,friendsList[i].scores);	
+		}
+
+		function differences(a1,a2){
+			var differences = 0;
+			for (var i=0;i<10;i++){
+				if (parseInt(a1[i]) != parseInt(a2[i])){
+					var diff = parseInt(a1[i]) - parseInt(a2[i]);
+					if (diff < 0){
+						diff = diff*(-1);
+					}
+					differences += diff;
+				}
+			}
+			diffArray.push(differences);
+		};
+
+		var lowest = Math.min.apply(null, diffArray);
+		for (var i=0;i<diffArray.length;i++){
+			if (lowest == diffArray[i]){
+				var match = friendsList[i];
+			}
+		}
+
+		res.json(match);
+
+	});
 };
+
+
 
